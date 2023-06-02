5b19007b7398987d848f6ca62788ee710ffb5914
Jolanoff <jawlan.abozaid12@gmail.com>
2023-06-02 11:02:32 +0200

    scuffed filter added but it works

--- front-end/src/pages/tools/FilterForm.js
+++ front-end/src/pages/tools/FilterForm.js
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Dropdown, Menu, Space, Button, Input } from 'antd';
+import { Dropdown, Menu, Space, Button, Input, Tag } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import axios from 'axios';
 
 const FilterForm = ({ handleSelectFilter }) => {
   const [filters, setFilters] = useState({});
-  const [searchText, setSearchText] = useState({
-    degree: '',
-    department: '',
-    subsystem: ''
-  });
+  const [selectedFilter, setSelectedFilter] = useState({ filterType: '', filterValue: '' });
+  const [searchText, setSearchText] = useState({ degree: '', department: '', subsystem: '' });
 
   useEffect(() => {
     const fetchFilters = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/filters`, {
-        });
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/filters`, {});
         setFilters(response.data);
       } catch (error) {
         console.error(error);
@@ -33,11 +29,22 @@ const FilterForm = ({ handleSelectFilter }) => {
     e.stopPropagation();
   };
 
+  const handleFilterSelect = (filterType, filterValue) => {
+    setSelectedFilter({ filterType, filterValue });
+    handleSelectFilter({ filterType, filterValue });
+  };
+
+  const handleFilterReset = () => {
+    setSelectedFilter({ filterType: '', filterValue: '' });
+    handleSelectFilter({ filterType: '', filterValue: '' });
+  };
+
   const renderMenuItem = (item, filterType) => (
     item.toLowerCase().includes(searchText[filterType].toLowerCase()) && (
       <Menu.Item
+
         key={item}
-        onClick={() => handleSelectFilter({ filterType, filterValue: item })}
+        onClick={() => handleFilterSelect(filterType, item)}
       >
         {item}
       </Menu.Item>
@@ -47,7 +54,7 @@ const FilterForm = ({ handleSelectFilter }) => {
   const renderSearch = (filterType) => (
     <Menu.Item key="search" onClick={e => e.stopPropagation()}>
       <input
-      className='h-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-12 py-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+        className='h-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-12 py-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
         placeholder={`Search ${filterType}`}
         onChange={onSearchChange(filterType)}
         onClick={handleInputClick}
@@ -55,32 +62,48 @@ const FilterForm = ({ handleSelectFilter }) => {
     </Menu.Item>
   );
 
+  const ScrollableMenu = ({ items, filterType }) => (
+    <div className='overflow-y-auto max-h-48'>
+      {items.map(item => renderMenuItem(item, filterType))}
+    </div>
+  );
+
   const menu = (
     <Menu>
-      <Menu.SubMenu key="group1" title="Degree">
+      <Menu.SubMenu key="degreeGroup" title="Degree">
         {renderSearch('degree')}
-        {filters.degrees?.map(item => renderMenuItem(item, 'degree'))}
+        <Menu.Divider />
+        <ScrollableMenu items={filters.degrees} filterType='degree' />
       </Menu.SubMenu>
-      <Menu.SubMenu key="group2" title="Department">
+      <Menu.SubMenu key="departmentGroup" title="Department">
         {renderSearch('department')}
-        {filters.departments?.map(item => renderMenuItem(item, 'department'))}
+        <Menu.Divider />
+        <ScrollableMenu items={filters.departments} filterType='department' />
       </Menu.SubMenu>
-      <Menu.SubMenu key="group3" title="Subsystem">
+      <Menu.SubMenu key="subsystemGroup" title="Subsystem">
         {renderSearch('subsystem')}
-        {filters.subsystems?.map(item => renderMenuItem(item, 'subsystem'))}
+        <Menu.Divider />
+        <ScrollableMenu items={filters.subsystems} filterType='subsystem' />
       </Menu.SubMenu>
     </Menu>
   );
 
   return (
-    <Dropdown overlay={menu} trigger={['click']}>
-      <Button>
-        <Space>
-          Filter
-          <DownOutlined />
-        </Space>
-      </Button>
-    </Dropdown>
+    <Space>
+      <Dropdown overlay={menu} trigger={['click']}>
+        <Button>
+          <Space>
+            Filter
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
+      {selectedFilter.filterType && selectedFilter.filterValue && (
+        <Tag closable onClose={handleFilterReset}>
+          {`${selectedFilter.filterType}: ${selectedFilter.filterValue}`}
+        </Tag>
+      )}
+    </Space>
   );
 };
 
